e7d458479abd86c36bfaa394c69d70521443c93c
Rohan_Islam <rohanislam3669@gmail.com>
2024-06-03 04:41:16 +0600

    kl

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,7 @@
+let btnStatus = false;
 const Header = () => {
-  let student = ["rohan", "rihan", "rihab", "foysal"];
-
   return (
-    <div>
-      {student.map((item, i) => {
-        return <li key={i.toString()}>{item}</li>;
-      })}
-    </div>
+    <div>{btnStatus ? <button>LogOout</button> : <button>Login</button>}</div>
   );
 };
 
