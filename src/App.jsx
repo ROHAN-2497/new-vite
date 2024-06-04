8a74ac0d480f5e1a9a23531ae95b5d8ccac515ae
Rohan_Islam <rohanislam3669@gmail.com>
2024-06-05 00:59:24 +0600

    useRef HOOk with Css

--- src/App.jsx
+++ src/App.jsx
@@ -2,30 +2,15 @@ import React from "react";
 import { useRef } from "react";
 
 const App = () => {
-  let firstName,
-    lastName = useRef();
-
-  const changeName = () => {
-    const changeFirstName = firstName.value;
-    const changeLasttName = lastName.value;
-    alert(changeFirstName + " " + changeLasttName);
+  let number = useRef();
+  const changeNumber = () => {
+    number.current++;
   };
-
   return (
-    <div>
-      <input
-        ref={(firstname) => (firstName = firstname)}
-        type="text"
-        placeholder="First Name"
-      />{" "}
-      <br />
-      <input
-        ref={(lastname) => (lastName = lastname)}
-        type="any"
-        placeholder="Last Name"
-      />{" "}
-      <br />
-      <button onClick={changeName}>Click</button>
+    <div className="m-20">
+      <button onClick={newColour} className="border p-2 bg-black text-white">
+        Click That
+      </button>
     </div>
   );
 };
