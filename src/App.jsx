58df78cba5b11f842dc516e546baca859d78ff6d
Rohan_Islam <rohanislam3669@gmail.com>
2024-06-05 11:11:36 +0600

    I

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,14 @@ import React from "react";
 import { useRef } from "react";
 
 const App = () => {
-  let number = useRef();
+  let number = useRef(0);
   const changeNumber = () => {
     number.current++;
+    console.log(number.current);
   };
   return (
     <div className="m-20">
-      <button onClick={newColour} className="border p-2 bg-black text-white">
+      <button onClick={changeNumber} className="border p-2 bg-black text-white">
         Click That
       </button>
     </div>
