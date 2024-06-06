c59dfd300b7aef65e1afbc9017816e43a7fc27ef
Rohan_Islam <rohanislam3669@gmail.com>
2024-06-07 03:35:34 +0600

    k

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,25 @@
 import React, { useState } from "react";
 
 const App = () => {
-  let [number, setNumber] = useState(0);
-  const increaceNumber = () => {
-    setNumber(number + 1);
+  let [objValue, setObjvalue] = useState({
+    key1: "value 1",
+    key2: "value 2",
+    key3: "value 3",
+  });
+  const changeValue = () => {
+    setObjvalue((prevObj) => ({
+      ...prevObj,
+      key1: "new value 1",
+    }));
   };
   return (
-    <div>
-      <h1>Number : {number}</h1>
-      <button onClick={increaceNumber} className="px-2 bg-black text-white">
-        Increace Number
+    <div className="m-20">
+      <h1>Object:{objValue.key1} </h1>
+      <button
+        onClick={changeValue}
+        className="px-1 border-red-200 border-2 bg-black text-white"
+      >
+        Click
       </button>
     </div>
   );
