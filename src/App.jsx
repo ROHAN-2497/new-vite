e1dafe502000cf62aa95315b4485687f8acffe87
Rohan_Islam <rohanislam3669@gmail.com>
2024-06-07 12:36:07 +0600

    kl

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,25 @@
-import React, { useState } from "react";
+import React from "react";
+import { useState } from "react";
 
 const App = () => {
-  let [objValue, setObjvalue] = useState({
-    key1: "value 1",
-    key2: "value 2",
-    key3: "value 3",
-  });
-  const changeValue = () => {
-    setObjvalue((prevObj) => ({
-      ...prevObj,
-      key1: "new value 1",
-    }));
+  let [list, setList] = useState([]);
+  let [item, setItem] = useState("");
+  const addList = () => {
+    list.push(item);
+    setList([...list]);
   };
+
   return (
-    <div className="m-20">
-      <h1>Object:{objValue.key1} </h1>
-      <button
-        onClick={changeValue}
-        className="px-1 border-red-200 border-2 bg-black text-white"
-      >
-        Click
+    <div className="">
+      <p>{list.length}</p>
+      <input
+        className="bg-black text-white"
+        placeholder=""
+        onChange={(e) => setItem(e.target.value)}
+        type="text"
+      />
+      <button className="border-2 px-2 bg-amber-400" onClick={addList}>
+        Add
       </button>
     </div>
   );
