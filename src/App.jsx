555911b9ac4d48c5555cdbdae88b051f6eed76c5
Rohan_Islam <rohanislam3669@gmail.com>
2024-06-09 20:26:48 +0600

    kl

--- src/App.jsx
+++ src/App.jsx
@@ -1,52 +1,17 @@
 import React from "react";
-import { useState } from "react";
 
 const App = () => {
-  let [list, setList] = useState([]);
-  let [item, setitem] = useState("");
-  const addToList = () => {
-    list.push(item);
-    setList([...list]);
-  };
-
-  const removeItem = (index) => {
-    list.splice(index, 1);
-    setList([...list]);
-  };
   return (
-    <div>
-      <table>
-        <tbody>
-          {list.length !== 0 ? (
-            list.map((element, index) => {
-              return (
-                <tr>
-                  <td>{element}</td>
-                  <td>
-                    {" "}
-                    <button
-                      onClick={() => {
-                        removeItem(index);
-                      }}
-                    >
-                      RemoveItem
-                    </button>
-                  </td>
-                </tr>
-              );
-            })
-          ) : (
-            <tr></tr>
-          )}
-        </tbody>
-      </table>{" "}
-      <input
-        onChange={(e) => setitem(e.target.value)}
-        type="text"
-        name=""
-        id=""
-      />
-      <button onClick={addToList}>Click</button>
+    <div className="container">
+      <form action="">
+        <input type="text" name="" id="" placeholder="first Name" />
+        <input type="text" name="" id="" placeholder="last Name" />
+        <select name="" id="">
+          <option value="">Chose City</option>
+          <option value="Dhaka">Dhaka</option>
+          <option value="Rangpur">Rangpur</option>
+        </select>
+      </form>
     </div>
   );
 };
